ad4957032aa1658b5e247b997ac7fe2550bafcdb
Yanina Bucca <yani.bucca@gmail.com>
2021-07-09 00:22:42 -0300

    sweet alert in control

--- script.js
+++ script.js
@@ -145,8 +145,8 @@ const selectLevel = () => {
     });
             
 };
-//######### MENSAJE DE INICIO ######
 
+//######### MENSAJE DE INICIO ######
     Swal.fire({
         title: '¡Bienvenida!',
         html: '<p class="modal">En MatcheADAs tu objetivo es juntar tres o más ítems del mismo tipo,ya sea en fila o columna. Para eso, selecciona un ítem y a continuación un ítem adyacente para intercambiarlos de lugar.<br>Si se forma un grupo, esos ítems se eliminarán y ganarás puntos.¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!</p><br><span class="modal">CONTROLES</span><p class="modal">Click izquierdo: selección</p><p class="modal">Enter o Espacio: selección</p><p class="modal">Flechas o WASD: movimiento e intercambio</p>',
@@ -164,15 +164,53 @@ const selectLevel = () => {
         selectLevel();
     });
 
-// //######### MENSAJE DE INFORMACION ######
-// const info = document.getElementById('info');
-
-// const infoBtn = () => {
-
     
+// ######### MENSAJE DE INFORMACION ######
+const info = document.getElementById('info');
 
+const infoBtn = () => {
+    Swal.fire({
+        title: '¡Bienvenida!',
+        html: '<p class="modal">En MatcheADAs tu objetivo es juntar tres o más ítems del mismo tipo,ya sea en fila o columna. Para eso, selecciona un ítem y a continuación un ítem adyacente para intercambiarlos de lugar.<br>Si se forma un grupo, esos ítems se eliminarán y ganarás puntos.¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!</p><br><span class="modal">CONTROLES</span><p class="modal">Click izquierdo: selección</p><p class="modal">Enter o Espacio: selección</p><p class="modal">Flechas o WASD: movimiento e intercambio</p>',
+        confirmButtonText: 'JUGAR',
+        padding:'1rem',
+        backdrop:true,
+        customClass: {
+            confirmButton: 'btn-play'
+        },
+        showConfirmButton: true,
+        confirmButtonColor: '#f87372',
+        confirmButtonAriaLabel: 'Iniciar el juego',
+    });
+    
+}
+info.addEventListener('click', infoBtn);
+// ######### MENSAJE DE REINICIAR JUEGO ######
+const restartBtn = document.getElementById('restart')
+const restarted = () => {
 
-// }
+    swal({
+        title: '¿Reiniciar Juego?',
+        text: 'Perderás el puntaje acumulado', 
+        buttons: {
+            aceptar: {
+                text:'Nuevo Juego',
+                className: 'btn-play',
+            },
+            cancelar: {
+                text:'Cancelar',
+                className: 'btn-play',
+            },
+        }
+    }).then((value) => {
+        if (value === 'aceptar') {
+            // selectLevel(); VER PORQUE DUPLICA ?
+        } else {
+            //cancelar
+        }
+    });
+}
+restartBtn.addEventListener('click', restarted)
 
 
 
@@ -384,4 +422,3 @@ const remove = () => {
 // }
 
 // }
-
