fb3bd8e532cea9faa6ed11d52b6e1c1c2923090d
Yanina Bucca <yani.bucca@gmail.com>
2021-07-11 21:58:35 -0300

    fix-message

--- script.js
+++ script.js
@@ -185,20 +185,7 @@ const selectLevel = () => {
 const info = document.getElementById('info');
 
 const infoBtn = () => {
-    Swal.fire({
-        title: '¡Bienvenida!',
-        html: '<p class="modal">En MatcheADAs tu objetivo es juntar tres o más ítems del mismo tipo,ya sea en fila o columna. Para eso, selecciona un ítem y a continuación un ítem adyacente para intercambiarlos de lugar.<br>Si se forma un grupo, esos ítems se eliminarán y ganarás puntos.¡Sigue armando grupos de 3 o más antes de que se acabe el tiempo!</p><br><span class="modal">CONTROLES</span><p class="modal">Click izquierdo: selección</p><p class="modal">Enter o Espacio: selección</p><p class="modal">Flechas o WASD: movimiento e intercambio</p>',
-        confirmButtonText: 'JUGAR',
-        padding:'1rem',
-        backdrop:true,
-        customClass: {
-            confirmButton: 'btn-play'
-        },
-        showConfirmButton: true,
-        confirmButtonColor: '#f87372',
-        confirmButtonAriaLabel: 'Iniciar el juego',
-    });
-    
+    window.location.reload(); 
 }
 info.addEventListener('click', infoBtn);
 
@@ -226,7 +213,8 @@ const restarted = () => {
             selectLevel();
             
         } else {
-            window.location.reload();
+            // window.location.reload();
+            //cancelar
         }
     });
 }
