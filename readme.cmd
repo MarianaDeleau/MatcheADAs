57a5322b5f946fc0f785b439ffafa7cfb580ccb3
grasielarosa <grasielarosaa@gmail.com>
2021-07-15 14:49:34 +0200

    first readme review

--- readme.cmd
+++ readme.cmd
@@ -4,22 +4,23 @@
 ### Trabajo Práctico Nº 3 
 
 ### Desarrolladoras: 
+- Belen Cludius: [portafólio] (https://belencludius.github.io/portafolio-ada/)
 - Grasiela da Rosa: [portafólio] (https://grasielarosa.github.io/ada-portfolio-project/)
 - Mariana Deleau: [portafólio] (https://marianadeleau.github.io/ada-8va-portafolio/)
-- Belen Cludius: [portafólio] (https://belencludius.github.io/portafolio-ada/)
 - Yanina Bucca: [portafólio] (https://ybucca.github.io/PortafolioYB/)
 
 ### Observaciones del trabajo:
-- se realizó una branch por cada función 
-- se utilizó la librería Twemoji
-- utilizamos librería SweetAlert para modales
-- diseño web responsive para mobile 
+- proyecto realizado como trabajo práctico de la carrera de front end de ADA ITw, aplicando los conocimientos adquiridos hasta el momento.
+- Además de poner en práctica los conocimientos de javaScript, también incorporamos lo aprendido sobre versionamiento en git;
+- se utilizó la librería Twemoji;
+- utilizamos librería SweetAlert para modales;
+- diseño web responsive para mobile.
 
 ## Abrirlo con Live server.
 
 ## Juego :
-- cuenta con tres niveles de dificultad
-- elimina grupos de forma vertical y horizontal de a tres o más coincidencias 
-- suma 100 puntos por cada item
-- el tiempo de juego es de 30 segundos
+- cuenta con tres niveles de dificultad;
+- elimina grupos de forma vertical y horizontal de a tres o más coincidencias; 
+- suma 100 puntos por cada item;
+- el tiempo de juego es de 30 segundos;
 
