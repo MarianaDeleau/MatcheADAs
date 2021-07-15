bb3d393bb9a30e02c4bf265600ddcaf2c5acf748
Yanina Bucca <yani.bucca@gmail.com>
2021-07-14 21:49:46 -0300

    add readme matcheadas

--- readme.cmd
+++ readme.cmd
@@ -5,11 +5,11 @@
 
 ### Desarrolladoras: 
 - Grasiela da Rosa: [portafólio] (https://grasielarosa.github.io/ada-portfolio-project/)
-- Mariana Delau: [portafólio] (https://marianadeleau.github.io/ada-8va-portafolio/)
+- Mariana Deleau: [portafólio] (https://marianadeleau.github.io/ada-8va-portafolio/)
 - Belen Cludius: [portafólio] (https://belencludius.github.io/portafolio-ada/)
 - Yanina Bucca: [portafólio] (https://ybucca.github.io/PortafolioYB/)
 
-### Observaciones para leer el código:
+### Observaciones:
 - se realizó una branch por cada función 
 - se utilizó la librería Twemoji
 - utilizamos librería SweetAlert para modales
